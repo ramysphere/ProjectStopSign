1bb77b327c95bb302131aaccb39e4dbdc22e12ad
Hayder Sadig <Hayder Sadig@SADIG-HPLAPTOP>
2022-09-21 23:44:00 -0400

    Add project files.

---
+++ ProjectStopSign/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectStopSign
+{
+    internal class Program
+    {
+        class StopSign
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! This is a stop sign.");
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("   XXXXXX");
+            Console.WriteLine(" X        X");
+            Console.WriteLine("X   STOP   X");
+            Console.WriteLine(" X        X");
+            Console.WriteLine("   XXXXXX");
+            Console.WriteLine("      X");
+            Console.WriteLine("      X");
+            Console.WriteLine("      X");
+
+            Console.WriteLine("-------------");
+        }
+
+    }
+}
